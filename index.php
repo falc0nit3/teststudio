8a11ea09e222d2ffb62b7a1adddaecbe2c366747
falc0nit3 <rohith.sunny@gmail.com>
2014-11-14 03:02:49 -0800

    Initial Checkin

---
+++ index.php
@@ -0,0 +1,249 @@
+
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US" prefix="og: http://ogp.me/ns#">
+<head>
+    <meta charset=UTF-8/>
+    <title>The Review Monk &laquo; Discover, Review and Rate Indian Movies! </title>
+
+    <style>
+        #header-top {
+            border-bottom: 1px solid #B8B8B8;
+            background: #F0F0F4;
+            height: 70px;
+            position: fixed;
+            top: 0;
+            background-image: -webkit-linear-gradient(top, #F0F0F4 0%, #D0D0D4 100%);
+            width: 100%;
+            z-index: 100;
+        }
+
+        #header-container {
+            width: 1102px;
+            margin: 0 auto;
+        }
+
+        #logo {
+            float: left;
+            position: relative;
+            top: 1px;
+            left: -5px;
+            width: 64px;
+            height: 67px;
+        }
+
+        #logo a {
+            float: left;
+        }
+
+        #logo-text img {
+            height: 60px;
+            padding-top: 8px;
+        }
+
+        #logo-text {
+            float: left;
+        }
+
+        #menu {
+            float: left;
+            width: 230px;
+            margin: 23px 0 0 0px;
+            border-radius: 6px;
+            padding-left: 20px;
+        }
+
+        #menu li {
+            float: left;
+            position: relative;
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+        }
+
+        #menu a {
+            float: left;
+            color: #000;
+            text-transform: uppercase;
+            font: 14px Arial, Helvetica;
+            text-decoration: none;
+        }
+
+        header-link {
+            padding: 10px 10px;
+            letter-spacing: 1px;
+        }
+
+        #menu ul {
+            margin: 20px 0 0 0;
+            _margin: 0;
+            opacity: 0;
+            visibility: hidden;
+            position: absolute;
+            top: 33px;
+            left: 0;
+            z-index: 1;
+            background: #EAF2F5;
+            box-shadow: 0 -1px 0 rgba(255,255,255,.3);
+            -webkit-transition: all .2s ease-in-out;
+            float: left;
+            width: 282px;
+            border-bottom-left-radius: 4px;
+            border-bottom-right-radius: 4px;
+            border-top-right-radius: 4px;
+            border: 1px solid #CCC;
+        }
+
+        #menu ul li {
+            float: left;
+            display: block;
+            border: 0;
+        }
+
+        #menu li {
+            float: left;
+            position: relative;
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+        }
+
+        #menu ul a {
+            padding: 7px;
+            width: 80px;
+            _height: 10px;
+            display: block;
+            white-space: nowrap;
+            float: none;
+            text-transform: none;
+            text-align: center;
+        }
+
+        #menu ul li:first-child > a {
+            border-radius: 0px 0px 0px 4px;
+        }
+
+        #login-wrapper {
+            margin: 30px 0 0 0px;
+            float: right;
+            text-transform: uppercase;
+            font: normal 15px Arial, Helvetica;
+        }
+
+
+
+        #page-outer-wrapper {
+            padding-bottom: 300px;
+            padding-top: 81px;
+        }
+
+        #page-wrapper {
+            margin: 0px auto 10px auto;
+            width: 1090px;
+            text-align: left;
+            padding: 0 5px 5px 5px;
+            webkit-box-shadow: 0 0 4px rgba(0,0,0,.3);
+            -moz-box-shadow: 0 0 4px rgba(0,0,0,.3);
+            box-shadow: 0 0 4px rgba(0,0,0,.3);
+        }
+
+        #content-wrapper {
+            float: left;
+            width: 100%;
+            position: relative;
+        }
+
+        #footer{
+            background:#888;
+            width: 100% !important;
+            height: 100px;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    </style>
+
+</head>
+
+<body class="home-page buddypress home blog bp-adminbar">
+
+
+<div id="main-container" class="">
+    <div id="header-top">
+        <div id="header-container">
+            <!--Begin Logo-->
+            <div id="logo">
+                <a href="http://localhost:8888/thereviewmonk"><img src="http://localhost:8888/thereviewmonk/assets/img/TRM_Logo_10.png" alt=""></a>
+            </div>
+            <div id="logo-text">
+                <a href="http://localhost:8888/thereviewmonk"><img src="http://localhost:8888/thereviewmonk/assets/img/text_logo_2.png " alt=""></a>
+            </div>
+            <!--End Logo-->
+
+            <ul id="menu">
+                <li class="li-header">
+                    <a href="javascript:void(0)" class="header-link">Movies</a>
+                    <ul>
+                        <li id="menu-running"><a href="http://localhost:8888/thereviewmonk/movies/running">Now Playing</a></li>
+                        <li id="menu-upcoming"><a href="http://localhost:8888/thereviewmonk/movies/upcoming">Upcoming</a></li>
+                        <li id="menu-toprated"><a href="http://localhost:8888/thereviewmonk/movies/toprated">Top Rated</a></li>
+                    </ul>
+                </li>
+                <li class="li-header">
+                    <a href="http://localhost:8888/thereviewmonk/celebrities" class="header-link">Celebrities</a>
+                </li>
+            </ul>
+
+            <form id="searchform" action="http://localhost:8888/thereviewmonk" method="get">
+                <input type="submit" id="searchsubmit" value="#" class="iconnew">
+                <input type="text" name="s" id="searchbarfield" placeholder="Search movies and celebs..." style="width: 230px;" autocomplete="off" tabindex="1" class="idleField idleField_login">
+            </form>
+
+
+            <!--Social Icons-->
+            <div id="social-icons">
+                <a href="https://www.facebook.com/TheReviewMonk" target="_blank" title="Connect to Facebook" class="ss-facebook"></a>
+                <a href="https://twitter.com/thereviewmonk" target="_blank" title="Connect to Twitter" class="ss-twitter"></a>
+                <a href="//thereviewmonk.tumblr.com/" target="_blank" title="Connect to Tumblr" class="ss-tumblr"></a>
+            </div>
+
+            <div id="login-wrapper">
+                <div id="reg-login">
+                    <a href="#login-box" data-rel="prettyPhoto" rel="prettyPhoto">Login</a>
+                </div>
+
+                <div id="reg-register">
+                    <a href="http://localhost:8888/thereviewmonk/register">Register</a>
+                </div>
+
+            </div>
+
+        </div><!--#header-container-->
+    </div>
+
+
+    <div id="page-outer-wrapper" style="padding-top:71px;">
+        <div id="page-wrapper" style="">
+            <div id="content-wrapper">
+                asda;dk;lskd;askd;as
+            </div>
+        </div>
+    </div>
+
+    <div id="footer" style="">
+        asds
+    </div>
+
+
+
+</div>
+
+
+</body>
+</html>
